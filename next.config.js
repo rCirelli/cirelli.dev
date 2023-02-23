cb66f4a08caddfcd9452cf50372feec9f8c1a57a
Raphael Cirelli <raphael.cirelli@protonmail.com>
2023-02-23 17:36:25 -0300

    disable Next image optimization to be able to use 'next export' in bohr.io

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: { //remove this when possible
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
